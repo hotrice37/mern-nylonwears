318bb1a5e77256050563eca2310a65e93eae78fa
hotrice37 <shubinpokhrel4@gmail.com>
2022-11-20 08:51:02 +0545

    created server

---
+++ backend/data.js
@@ -0,0 +1,46 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      img: '../images/man1.png',
+      prodname: 'Nope Not Today Hoodie',
+      price: 1150,
+      numReviews: 10,
+      rating: 3.5,
+      slug: 'nope-not-today-hoodie',
+      stock: 10,
+    },
+    {
+      id: '2',
+      img: '../images/women1.png',
+      prodname: 'Stiched Ladies Kurti',
+      price: 595,
+      numReviews: 5,
+      rating: 2,
+      slug: 'stitiched-ladies-kurti',
+      stock: 20,
+    },
+    {
+      id: '3',
+      img: '../images/kid1.png',
+      prodname: 'Baby Suit Unisex',
+      price: 750,
+      numReviews: 5,
+      rating: 3,
+      slug: 'baby-suit-unisex',
+      stock: 20,
+    },
+    {
+      id: '4',
+      img: '../images/boot.png',
+      prodname: 'Nylon Boot Predator',
+      price: 13500,
+      numReviews: 11,
+      rating: 4.5,
+      slug: 'nylon-boot-predator',
+      stock: 20,
+    },
+  ],
+};
+
+export default data;
