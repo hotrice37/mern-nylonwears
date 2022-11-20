b392cea2b77d38d4ba148245060d7dedb8f19798
hotrice37 <shubinpokhrel4@gmail.com>
2022-11-20 17:15:40 +0545

    created sample user

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Admin',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('12345678'),
+      isAdmin: true,
+    },
+    {
+      name: 'John',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('12345678'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       prodname: 'Nope Not Today Hoodie',
