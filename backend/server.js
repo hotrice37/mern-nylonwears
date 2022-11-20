9d06c1da4a5c4935d8ba1a9f62be9c5af6218f16
hotrice37 <shubinpokhrel4@gmail.com>
2022-11-20 15:04:05 +0545

    connected to mongodb atlas

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,18 @@
 import express from 'express';
 import data from './data.js';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 const app = express();
 
